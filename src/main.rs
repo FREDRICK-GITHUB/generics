4065626ea81c8cc7c8c63b358f580207ea0222e6
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-12-08 08:49:31 +0300

    function having generic type in its signature example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-fn largest (list: &[i32]) -> &i32 {
+fn largest<T> (list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
@@ -19,20 +19,11 @@ fn largest (list: &[i32]) -> &i32 {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    // let mut largest = &number_list[0];
-
-    // for number in &number_list {
-    //     if number > largest {
-    //         largest = number;
-    //     }
-    // }
-    // println!("The largest number is: {}", largest);
-
     let result = largest(&number_list);
-    println!("The largest number is: {}", result);
+    println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&number_list);
-    println!("The largest number is: {}", result);
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
