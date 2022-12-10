50f93a568fe65ef526f7a8c80dac93f59c41d704
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-12-11 00:11:18 +0300

    lifetime annotations for structs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
-/*lifetime annotation for functions. the lifetime of the returned type is equivalent to the shortest lifetime of the function arguments/signature */
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    }else {
-        y
-    }
+/*lifetime annotation for structs. */
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
 
 fn main() {
-    let string1 =  String::from("long string is long");
-    
-    {
-        let string2 = "xyz";
-
-        let result = longest(string1.as_str(), string2);
-        println!("The longest string between the two is {}", result);
-    }
-    
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("Valid part is: {}", i.part);
 }
