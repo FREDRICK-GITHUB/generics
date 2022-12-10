72111550a7dfbb08a8ebeeccb69528b1ab5be9f0
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-12-11 00:21:27 +0300

    generics, traits and lifetimes summary

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-/*lifetime annotation for structs. */
-struct ImportantExcerpt<'a> {
-    part: &'a str,
-}
+/*example that covers generics, traits and lifetimes all in one function */
+use std::fmt::Display;
 
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
 
 fn main() {
     let novel = String::from("Call me Ishmael. Some years ago...");
-    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
-    let i = ImportantExcerpt {
-        part: first_sentence,
-    };
-    println!("Valid part is: {}", i.part);
+    let first_word = "call";
+
+    let response = longest_with_an_announcement(&novel, &first_word, "Breaking news!");
+    
+    println!("Response is: {}", response);
 }
