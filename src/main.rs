c3a9971b051e1c313df214fd4da28709b1dd35eb
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-12-10 21:18:53 +0300

    lifetime annotation for functions

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-
-struct Point<T> {
-    x: T,
-    y: T,
-}
-
-/* example implementation of generic types in methods */
-impl<T> Point<T> {
-    fn x(&self)-> &T {
-        &self.x
+/*lifetime annotation for functions. the lifetime of the returned type is equivalent to the shortest lifetime of the function arguments/signature */
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    }else {
+        y
     }
 }
 
 
 fn main() {
-    let p = Point { x:5, y:10 };
-    println!("p.x = {}", p.x());
+    let string1 =  String::from("long string is long");
+    
+    {
+        let string2 = "xyz";
+
+        let result = longest(string1.as_str(), string2);
+        println!("The longest string between the two is {}", result);
+    }
+    
 }
